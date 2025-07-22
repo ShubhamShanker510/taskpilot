a33dc6aeefab636d632e37838d23803e32c38f1d
ShubhamShanker510 <ssshanker2001@gmail.com>
2025-07-22 18:41:50 +0530

    task and project controller conversion from fbv to cbv

--- projects/views/project_views.py
+++ projects/views/project_views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.cache import cache
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from django.views import View
 
 from ..models import *
 from ..forms import *
@@ -11,75 +15,154 @@ from ..services import project_services
 
 
 # create and edit project
-@login_required(login_url='/users/login/')
-def create_edit_project(request, project_id=None):
-    project=project_services.get_project_by_id_with_cache(project_id) if project_id else None
+class create_edit_project(LoginRequiredMixin, View):
+    login_url = '/users/login/'
+    template_name = 'dashboard/create_edit_project.html'
 
-    if request.method=="POST":
-        form=createProject(request.POST, instance=project)
+    def get_project(self, project_id):
+        return project_services.get_project_by_id_with_cache(project_id) if project_id else None
+
+    def get(self, request, project_id=None):
+        project = self.get_project(project_id)
+        form = createProject(instance=project)
+        return render(request, self.template_name, {'form': form, 'project': project})
+
+    def post(self, request, project_id=None):
+        project = self.get_project(project_id)
+        form = createProject(request.POST, instance=project)
 
         if form.is_valid():
-            new_project=form.save(commit=False)
+            new_project = form.save(commit=False)
 
             if not project:
-                new_project.created_by=request.user
-            
-            new_project.save()
-            cache.delete('dashboard_counts')
+                new_project.created_by = request.user
 
+            new_project.save()
+
+            # Clear cache
+            cache.delete('dashboard_counts')
             if project_id:
                 cache.delete(f"project:{project_id}")
-            
+
             if project:
                 messages.success(request, "Project updated successfully")
             else:
                 messages.success(request, "Project created successfully")
-            return redirect('/dashboard/projects')         
+
+            return redirect('/dashboard/projects')
+
+        return render(request, self.template_name, {'form': form, 'project': project})
+
+# @login_required(login_url='/users/login/')
+# def create_edit_project(request, project_id=None):
+#     project=project_services.get_project_by_id_with_cache(project_id) if project_id else None
+
+#     if request.method=="POST":
+#         form=createProject(request.POST, instance=project)
+
+#         if form.is_valid():
+#             new_project=form.save(commit=False)
+
+#             if not project:
+#                 new_project.created_by=request.user
+            
+#             new_project.save()
+#             cache.delete('dashboard_counts')
+
+#             if project_id:
+#                 cache.delete(f"project:{project_id}")
+            
+#             if project:
+#                 messages.success(request, "Project updated successfully")
+#             else:
+#                 messages.success(request, "Project created successfully")
+#             return redirect('/dashboard/projects')         
 
     
-    else:
-        form=createProject(instance=project)
+#     else:
+#         form=createProject(instance=project)
 
-    return render(request, 'dashboard/create_edit_project.html', {'form': form, 'project': project})
+#     return render(request, 'dashboard/create_edit_project.html', {'form': form, 'project': project})
 
 
 # all project list
-@login_required(login_url='/users/login/')
-def project_table(request):
+class project_table(LoginRequiredMixin, ListView):
+    login_url = '/users/login/'
+    template_name = 'dashboard/project_table.html'
 
-    filters={
-        'title': request.GET.get('title', '').strip(),
-        'role': request.GET.get('role', '').strip(),
-        'username': request.GET.get('username', '').strip(),
-    }
+    def get(self, request):
+        filters = {
+            'title': request.GET.get('title', '').strip(),
+            'role': request.GET.get('role', '').strip(),
+            'username': request.GET.get('username', '').strip(),
+        }
+        page_number = request.GET.get('page')
+
+        data = project_services.get_filtered_projects(filters, page_number)
+
+        for key, value in filters.items():
+            if value:
+                messages.info(request, f"Filtered by {key}: {value}")
+
+        context = {
+            'projects': data['page'],
+            'page_obj': data['page'],
+            'pagecount': data['pagecount'],
+            'selected_title': filters['title'],
+            'selected_role': filters['role'],
+            'selected_username': filters['username'],
+            'roles': data['roles'],
+        }
+
+        return render(request, self.template_name, context)
+
+# @login_required(login_url='/users/login/')
+# def project_table(request):
+
+#     filters={
+#         'title': request.GET.get('title', '').strip(),
+#         'role': request.GET.get('role', '').strip(),
+#         'username': request.GET.get('username', '').strip(),
+#     }
     
-    page_number = request.GET.get('page')
+#     page_number = request.GET.get('page')
 
-    data=project_services.get_filtered_projects(filters, page_number)
+#     data=project_services.get_filtered_projects(filters, page_number)
 
-    for key, value in filters.items():
-        if value.strip():
-            messages.info(request, f"Filtered by {key}: {value.strip()}")
+#     for key, value in filters.items():
+#         if value.strip():
+#             messages.info(request, f"Filtered by {key}: {value.strip()}")
 
-    return render(request, 'dashboard/project_table.html', {
-        'projects': data['page'],  
-        'page_obj': data['page'],
-        'pagecount': data['pagecount'],
-        'selected_title': filters['title'],
-        'selected_role': filters['role'],
-        'selected_username': filters['username'],
-        'roles': data['roles'],
-    })
+#     return render(request, 'dashboard/project_table.html', {
+#         'projects': data['page'],  
+#         'page_obj': data['page'],
+#         'pagecount': data['pagecount'],
+#         'selected_title': filters['title'],
+#         'selected_role': filters['role'],
+#         'selected_username': filters['username'],
+#         'roles': data['roles'],
+#     })
 
 
 # delete project by admin
-@login_required(login_url='/users/login/')
-def delete_project(request, project_id):
-    if request.user.role == 'admin':
-        project=get_object_or_404(Project.objects.select_related('created_by'), id=project_id)
-        project.delete()
-        messages.success(request, "Project deleted successfully")
-        cache.delete('dashboard_counts')
+class delete_project(LoginRequiredMixin, View):
+    login_url = '/users/login/'
+
+    def get(self, request, project_id):
+        if request.user.role == 'admin':
+            project = get_object_or_404(Project.objects.select_related('created_by'), id=project_id)
+            project.delete()
+            messages.success(request, "Project deleted successfully")
+            cache.delete('dashboard_counts')
         return redirect('/dashboard/projects/')
-    elif request.user.role =='manager':
-        return redirect('/dashboard/projects/')
+
+# @login_required(login_url='/users/login/')
+# def delete_project(request, project_id):
+#     if request.user.role == 'admin':
+#         project=get_object_or_404(Project.objects.select_related('created_by'), id=project_id)
+#         project.delete()
+#         messages.success(request, "Project deleted successfully")
+#         cache.delete('dashboard_counts')
+#         return redirect('/dashboard/projects/')
+#     elif request.user.role =='manager':
+#         return redirect('/dashboard/projects/')
--- tasks/views/task_views.py
+++ tasks/views/task_views.py
@@ -4,6 +4,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.core.cache import cache
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
+from django.views import View
 
 from ..services import task_services, comment_services
 from ..tasks import *
@@ -11,15 +15,26 @@ from ..models import *
 from ..forms import *
 from users.models import *
 
-@login_required(login_url='/users/login/')
-def create_edit_task(request, task_id=None):
+class create_edit_task(LoginRequiredMixin, View):
+    template_name='dashboard/create_edit_task.html'
+    login_url='/users/login'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.role == 'employee':
+            return redirect('/dashboard/tasks/')
+        return super().dispatch(request, *args, **kwargs)
     
-    if request.user.role == 'employee':
-        return redirect('/dashboard/tasks/')
+    def get_task(self, task_id=None):
+        return task_services.get_task_with_cache(task_id) if task_id else None
+    
+    def get(self, request, task_id=None):
+        task=self.get_task(task_id)
+        form=TaskForm(instance=task)
 
-    task=task_services.get_task_with_cache(task_id) if task_id else None
-
-    if request.method == 'POST':
+        return render(self.request,self.template_name , {'form': form, 'task': task})
+    
+    def post(self, request, task_id=None):
+        task=self.get_task(task_id)
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             task_obj = form.save(commit=False)
@@ -37,19 +52,51 @@ def create_edit_task(request, task_id=None):
             return redirect('/dashboard/tasks/')
         else:
             messages.warning(request, "Please enter valid data")
-    else:
-        form = TaskForm(instance=task)
 
-    return render(request, 'dashboard/create_edit_task.html', {'form': form, 'task': task})
+        return render(self.request, self.template_name, {'form': form, 'task': task})
+
+# @login_required(login_url='/users/login/')
+# def create_edit_task(request, task_id=None):
+    
+#     if request.user.role == 'employee':
+#         return redirect('/dashboard/tasks/')
+
+#     task=task_services.get_task_with_cache(task_id) if task_id else None
+
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST, instance=task)
+#         if form.is_valid():
+#             task_obj = form.save(commit=False)
+
+#             if task_obj.due_date > task_obj.project.deadline:
+#                 form.add_error('due_date', 'Due date cannot be after the project deadline.')
+#             else:
+#                 task_services.save_task_and_notify(task_obj, task_id)
+                
+#                 if task:
+#                     messages.success(request, 'Task updated successfully. Email will be sent shortly.')
+#                 else:
+#                     messages.success(request, 'Task created successfully. Email will be sent shortly.')
+                        
+#             return redirect('/dashboard/tasks/')
+#         else:
+#             messages.warning(request, "Please enter valid data")
+#     else:
+#         form = TaskForm(instance=task)
+
+#     return render(request, 'dashboard/create_edit_task.html', {'form': form, 'task': task})
 
 # updating status of task
-@login_required(login_url='/users/login/')
-def status_update(request, task_id=None):
-    task = get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
+class status_update(LoginRequiredMixin, View):
+    login_url = '/users/login/'
 
-    if request.method == 'POST':
+    def get(self, request, task_id):
+        return redirect('/dashboard/tasks/')
+
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, id=task_id)
         new_status = request.POST.get('status')
-        
+
         if new_status in ['pending', 'in_progress', 'done']:
             task.status = new_status
             task.save()
@@ -60,67 +107,152 @@ def status_update(request, task_id=None):
 
         return redirect('/dashboard/tasks/')
 
-    return redirect('/dashboard/tasks/')
+
+# @login_required(login_url='/users/login/')
+# def status_update(request, task_id=None):
+#     task = get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
+
+#     if request.method == 'POST':
+#         new_status = request.POST.get('status')
+        
+#         if new_status in ['pending', 'in_progress', 'done']:
+#             task.status = new_status
+#             task.save()
+#             cache.delete('dashboard_counts')
+#             messages.success(request, f"Task status updated to {new_status}.")
+#         else:
+#             messages.error(request, "Invalid status.")
+
+#         return redirect('/dashboard/tasks/')
+
+#     return redirect('/dashboard/tasks/')
 
 # task list
-@login_required(login_url='/users/login/')
-def task_table(request):
+class task_table(LoginRequiredMixin, View):
+    login_url = '/users/login/'
+    template_name = 'dashboard/task_table.html'
 
-    filters={
-        'title': request.GET.get('title', '').strip(),
-        'role': request.GET.get('role', '').strip(),
-        'username': request.GET.get('username', '').strip(),
-        'status' : request.GET.get('status', '').strip()
-    }
-    page_number = request.GET.get('page')
+    def get(self, request):
+        filters = {
+            'title': request.GET.get('title', '').strip(),
+            'role': request.GET.get('role', '').strip(),
+            'username': request.GET.get('username', '').strip(),
+            'status': request.GET.get('status', '').strip(),
+        }
+        page_number = request.GET.get('page')
 
-    data=task_services.get_filtered_tasks(request.user, filters, page_number)
+        data = task_services.get_filtered_tasks(request.user, filters, page_number)
 
-    for key, value in filters.items():
-        if value.strip():
-            messages.info(request, f"Filtered by {key}: {value.strip()}")
+        for key, value in filters.items():
+            if value:
+                messages.info(request, f"Filtered by {key}: {value}")
+
+        context = {
+            'tasks': data['page'],
+            'page_obj': data['page'],
+            'taskcount': data['count'],
+            'selected_title': filters['title'],
+            'selected_role': filters['role'],
+            'selected_username': filters['username'],
+            'selected_status': filters['status'],
+            'roles': data['roles'],
+            'statuses': data['statuses'],
+        }
+        return render(request, self.template_name, context)
+# @login_required(login_url='/users/login/')
+# def task_table(request):
+
+#     filters={
+#         'title': request.GET.get('title', '').strip(),
+#         'role': request.GET.get('role', '').strip(),
+#         'username': request.GET.get('username', '').strip(),
+#         'status' : request.GET.get('status', '').strip()
+#     }
+#     page_number = request.GET.get('page')
+
+#     data=task_services.get_filtered_tasks(request.user, filters, page_number)
+
+#     for key, value in filters.items():
+#         if value.strip():
+#             messages.info(request, f"Filtered by {key}: {value.strip()}")
 
 
-    return render(request, 'dashboard/task_table.html', {
-        'tasks': data['page'],
-        'page_obj': data['page'],
-        'taskcount': data['count'],
-        'selected_title': filters['title'],
-        'selected_role': filters['role'],
-        'selected_username': filters['username'],
-        'selected_status': filters['status'],
-        'roles': data['roles'],
-        'statuses': data['statuses'],
-    })
+#     return render(request, 'dashboard/task_table.html', {
+#         'tasks': data['page'],
+#         'page_obj': data['page'],
+#         'taskcount': data['count'],
+#         'selected_title': filters['title'],
+#         'selected_role': filters['role'],
+#         'selected_username': filters['username'],
+#         'selected_status': filters['status'],
+#         'roles': data['roles'],
+#         'statuses': data['statuses'],
+#     })
 
 # delete task by id
-@login_required(login_url='/users/login/')
-def delete_task(request, task_id):
+class delete_task(LoginRequiredMixin, View):
+    login_url = '/users/login/'
 
-    if request.user.role == 'employee' or request.user.role == 'manager':
-        return redirect('/dashboard/tasks/')
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.role == 'manager':
+            return redirect('/dashboard/home/')
+        
+        if request.user.role == 'employee':
+            return redirect('/dashboard/tasks/')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, task_id):
+        
+        selected_page = request.GET.get('page', 1)
+        task = get_object_or_404(Task, id=task_id)
+        task.delete()
+        messages.success(request, "task deleted successfully")
+        cache.delete('dashboard_counts')
+        return redirect(f'/dashboard/tasks/?page={selected_page}')
+
+# @login_required(login_url='/users/login/')
+# def delete_task(request, task_id):
+
+#     if request.user.role == 'employee' or request.user.role == 'manager':
+#         return redirect('/dashboard/tasks/')
     
 
-    task=get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
-    task.delete()
-    messages.success(request, "Task deleted successfully")
-    cache.delete('dashboard_counts')
-    return redirect('/dashboard/tasks')
+#     task=get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
+#     task.delete()
+#     messages.success(request, "Task deleted successfully")
+#     cache.delete('dashboard_counts')
+#     return redirect('/dashboard/tasks')
 
 # task details by id
-@login_required(login_url='/users/login/')
-def task_detail(request, task_id):
-    task = get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
-    comments = task.comments.select_related('author').all()
-    form = CommentForm()
-    editing_comment = None
+class task_detail(LoginRequiredMixin, View):
+    login_url = '/users/login/'
+    template_name = 'dashboard/task_detail.html'
 
-    if 'edit' in request.GET:
-        editing_comment = comment_services.get_comment_for_task(task, request.GET.get('edit'))
-        form = CommentForm(instance=editing_comment)
+    def get(self, request, task_id):
+        task = get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
+        comments = task.comments.select_related('author').all()
+        editing_comment = None
 
-    if request.method == "POST":
+        edit_id = request.GET.get('edit')
+        if edit_id:
+            editing_comment = comment_services.get_comment_for_task(task, edit_id)
+            form = CommentForm(instance=editing_comment)
+        else:
+            form = CommentForm()
+
+        return render(request, self.template_name, {
+            'task': task,
+            'comments': comments,
+            'form': form,
+            'editing_comment': editing_comment,
+        })
+
+    def post(self, request, task_id):
+        task = get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
         form = CommentForm(request.POST)
+        comments = task.comments.select_related('author').all()
+
         if form.is_valid():
             new_comment = form.save(commit=False)
             new_comment.task = task
@@ -128,10 +260,39 @@ def task_detail(request, task_id):
             new_comment.save()
             return redirect('task_detail', task_id=task_id)
 
-    return render(request, 'dashboard/task_detail.html', {
-        'task': task,
-        'comments': comments,
-        'form': form,
-        'editing_comment': editing_comment,
-    })
+        # If form is invalid, show the same page with errors
+        return render(request, self.template_name, {
+            'task': task,
+            'comments': comments,
+            'form': form,
+            'editing_comment': None
+        })
+
+
+# @login_required(login_url='/users/login/')
+# def task_detail(request, task_id):
+#     task = get_object_or_404(Task.objects.select_related('assigned_to', 'project'), id=task_id)
+#     comments = task.comments.select_related('author').all()
+#     form = CommentForm()
+#     editing_comment = None
+
+#     if 'edit' in request.GET:
+#         editing_comment = comment_services.get_comment_for_task(task, request.GET.get('edit'))
+#         form = CommentForm(instance=editing_comment)
+
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             new_comment = form.save(commit=False)
+#             new_comment.task = task
+#             new_comment.author = request.user
+#             new_comment.save()
+#             return redirect('task_detail', task_id=task_id)
+
+#     return render(request, 'dashboard/task_detail.html', {
+#         'task': task,
+#         'comments': comments,
+#         'form': form,
+#         'editing_comment': editing_comment,
+#     })
 
